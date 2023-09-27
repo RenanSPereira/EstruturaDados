aa5a9a6c0b48757536cb165559a33cf92857ec3b
Renan Santos <renanspereira.dev@gmail.com>
2023-09-27 17:31:01 -0300

    corrigindo chamada de metodo

--- tests/Algoritmos.Test/AlgoritmosIterativosTest.cs
+++ tests/Algoritmos.Test/AlgoritmosIterativosTest.cs
@@ -7,13 +7,13 @@ public class AlgoritmosIterativosTest
     [Fact(DisplayName = "6 X 1 = 6")]
     public void Seis_X_1_Igual_6_Iterativo()
     {
-        Assert.Equal(6, Iterativo.Multiplicacao(6, 1));
+        Assert.Equal(6, Multiplicacao.Calcular(6, 1));
     }
 
     [Fact(DisplayName = "6 X 3 = 18")]
     public void Seis_X_3_Igual_18_Iterativa()
     {
-        Assert.Equal(18, Iterativo.Multiplicacao(6, 3));
+        Assert.Equal(18, Multiplicacao.Calcular(6, 3));
     }
 
     [Fact(DisplayName = "Fatorial de 0 deve ser 1")]
