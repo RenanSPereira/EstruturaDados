7563b4b51988b0fabb1028f11d95f9a095854fb4
Renan Santos <renanspereira.dev@gmail.com>
2023-09-29 18:34:15 -0300

    removendo comentário

--- tests/Algoritmos.Test/Iterativos/BuscaBinariaTest.cs
+++ tests/Algoritmos.Test/Iterativos/BuscaBinariaTest.cs
@@ -7,7 +7,7 @@ public class BuscaBinariaTest
     private readonly int[] _vetor = new int[9] { 1, 3, 7, 15, 21, 36, 78, 95, 106 };
 
     [Theory(DisplayName = "Deve Encontrar O Numero")]
-    // [InlineData(1, 1)]
+    [InlineData(1, 1)]
     [InlineData(36, 36)]
     public void Deve_Encontrar_O_Numero(int entrada, int resultado)
     {
